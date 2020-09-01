044c1ac59413e3cf22863a7e963982d805d1a20b
catnip <catnip@catnip.fyi>
2020-09-01 16:47:38 +0200

    + keybinds

--- init.vim
+++ init.vim
@@ -2,14 +2,6 @@
 lua require 'init'
 lua require 'keys'
 
-" easier buffer switching
-nnoremap <C-J> <C-W><C-J>
-nnoremap <C-K> <C-W><C-K>
-nnoremap <C-L> <C-W><C-L>
-nnoremap <C-H> <C-W><C-H>
-
-inoremap jk <ESC>
-
 " get highlighting group under cursor
 map <F10> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
 \ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
--- lua/keys.lua
+++ lua/keys.lua
@@ -1,7 +1,10 @@
 local key = vim.api.nvim_set_keymap
 
--- easy buffer switching
-
+-- easy buffer switching (crlt+direction to switch buffer)
+key('n', '<C-J>', '<C-W><C-J>', {noremap = true})
+key('n', '<C-K>', '<C-W><C-K>', {noremap = true})
+key('n', '<C-L>', '<C-W><C-L>', {noremap = true})
+key('n', '<C-H>', '<C-W><C-H>', {noremap = true})
 
 -- use jk for escape
 key('i', 'jk', '<ESC>', {noremap = true})
