5bbf6e04351ba2d49f7bf81d039e02d6c73f07aa
sum-catnip <catnip@catnip.fyi>
2020-09-06 01:03:06 +0200

    - old code comments

--- lua/keys.lua
+++ lua/keys.lua
@@ -8,11 +8,3 @@ bind('n', '<C-H>', '<C-W><C-H>', 'noremap')
 
 -- use jk for escape
 bind('i', 'jk', '<ESC>', 'noremap')
-
--- coc trigger completion
--- bind('i', '<Tab>', 'v:lua.coc_trigger_tab_completion()', 'noremap', 'silent', 'expr')
-
--- coc advance completion or de-tab
--- bind('i', '<S-Tab>', 'v:lua.coc_shifttab_advance_completions()', 'noremap', 'expr')
-
--- bind('i', '<cr>', 'v:lua.coc_cr_confirm_completion')
