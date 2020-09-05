49db878f97c16830b133ff89cb10f0f340620f29
sum-catnip <catnip@catnip.fyi>
2020-09-06 00:42:51 +0200

    added cmd option for not returning binds

--- lua/binds.lua
+++ lua/binds.lua
@@ -6,15 +6,29 @@ _bindfuncs = {}
 local bindfunc_i = 0
 
 return function(mode, lhs, rhs, ...)
+  -- for every optional argument we make a field in the opts table
+  -- the field will be set to true
+  -- we do this because the neovim api wants {thing1: true, thing2: true, ...}
   local opt = {}
   for _, a in ipairs({...}) do opt[a] = true end
 
+  -- if we pass in a lua function
   if type(rhs) == 'function' then
+    -- make a vim readable expression that will call our function
+    -- in the global _bindfuncs table
     bindfunc_i = bindfunc_i + 1
     local name = 'bindfunc_' .. tostring(bindfunc_i)
     _bindfuncs[name] = rhs
-    rhs = 'v:lua._bindfuncs.' .. name .. '()'
-    opt.expr = true
+    -- if we use `cmd` the function return value is ignored
+    if opt.cmd then
+      rhs = '<cmd>call v:lua._bindfuncs.' .. name .. '()<cr>'
+      -- remove cmd because its not actually valid as an option
+      opt.cmd = nil
+    else
+      -- otherwise we call it as an expression using the return value
+      rhs = 'v:lua._bindfuncs.' .. name .. '()'
+      opt.expr = true
+    end
   end
 
   return vim.api.nvim_set_keymap(mode, lhs, rhs, opt)
