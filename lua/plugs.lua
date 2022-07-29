4a35bf7b03bc4ec106c0ebf11471aec6835968a0
sum-catnip <catnip@catnip.fyi>
2022-07-29 18:18:59 +0200

    + just fixing the error by getting rid of stuff temporarily

--- lua/plugs.lua
+++ lua/plugs.lua
@@ -4,27 +4,23 @@ local vf = require('vfuncs')
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'psliwka/vim-smoothie'
-  use 'mhinz/vim-startify'
-  use { 'junegunn/fzf', run = function() vf.fzf.install() end }
-  use 'junegunn/fzf.vim'
-  use 'machakann/vim-sandwich'
-  use 'bluz71/vim-moonfly-colors'
+  use 'wbthomason/packer.nvim' -- lua plugin manager
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- syntax highlight
+  use 'psliwka/vim-smoothie' -- smooth scrolling
+  use 'mhinz/vim-startify' -- start menu
+  use 'machakann/vim-sandwich' -- surround selection with character
+  use 'bluz71/vim-moonfly-colors' -- color theme
   use 'kyazdani42/nvim-web-devicons'
-  use 'justinmk/vim-sneak'
-  use 'dag/vim-fish'
+  use 'justinmk/vim-sneak' -- s + 2 chars as a motion
+  use 'dag/vim-fish' -- fish shell syntax
   use 'neovim/nvim-lspconfig'
-  use 'nvim-lua/completion-nvim'
-  use 'cespare/vim-toml'
-  use 'gfanto/fzf-lsp.nvim'
-  use 'itchyny/lightline.vim'
-  use 'cohama/lexima.vim'
+  use 'cespare/vim-toml' -- toml syntax
+  use 'itchyny/lightline.vim' -- statusbar
+  use 'cohama/lexima.vim' -- auto close parenthesis
 end)
 
 require('plugs.startify')
-require('plugs.fzf')
+--require('plugs.fzf')
 require('plugs.sneak')
-require('plugs.lsp')
-require('plugs.completion-nvim')
+--require('plugs.lsp')
+--require('plugs.completion-nvim')
